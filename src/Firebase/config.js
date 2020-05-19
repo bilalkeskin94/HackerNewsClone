34c4ea7b0181694f4562589dceff67f7548b7a54
bilalkeskin <bilalkeskin_94@hotmail.com>
2020-05-19 13:37:26 +0300

    firebase setup

---
+++ src/Firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCxUEGJa8iEvx-Bck4cwgMQAdAByD9h2s",
+  authDomain: "hacker-news-clone-fd7bc.firebaseapp.com",
+  databaseURL: "https://hacker-news-clone-fd7bc.firebaseio.com",
+  projectId: "hacker-news-clone-fd7bc",
+  storageBucket: "hacker-news-clone-fd7bc.appspot.com",
+  messagingSenderId: "205513580357",
+  appId: "1:205513580357:web:a07e609286ef396a4f682c",
+  measurementId: "G-VL9GC4JEKS",
+};
+export default firebaseConfig;
